b49ff2df0ec77505c7b8bfe798fc42cb73fa9da6
mbournehalley <mbournehalley@gmail.com>
2019-01-31 23:00:23 -0800

    Wiring a form with validation.

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -7,9 +7,15 @@
 import React from 'react';
 
 import styles from './styles.css';
-
+import validator from 'email-validator';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  login = () => {
+    const email = this.emailField.value;
+    if (!validator.validate(email)) {
+      console.log('invalid email');
+    }
+  }
   render() {
     return (
       <div className={styles.login}>
@@ -24,7 +30,7 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
         />
         <div className={styles.actionContainer}>
           <div className={styles.button}>Cancel</div>
-          <div className={styles.button}>Login</div>
+          <div className={styles.button} onClick={this.login}>Login</div>
         </div>
       </div>
     );
