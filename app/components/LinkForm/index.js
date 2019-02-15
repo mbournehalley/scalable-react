bfe073480603e764de677aa6cdbef4f7c38bd2e5
mbournehalley <mbournehalley@gmail.com>
2019-02-14 21:19:12 -0800

    Display a component in a dialogue.

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -15,25 +15,27 @@ class LinkForm extends React.Component { // eslint-disable-line react/prefer-sta
   state = {};
   render() {
     return (
-      <div className={styles.LinkForm}>
-        <div className={styles.heading}>
-          Login with your email
-        </div>
-        <input
-          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
-          placeholder="URL"
-          ref={(f) => { this.urlField = f; }}
-          type="text"
-        />
-        <input
-          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
-          placeholder="Description"
-          ref={(f) => { this.descriptionField = f; }}
-          type="text"
-        />
-        <div className={styles.actionContainer}>
-          <div className={styles.button} onClick={this.props.cancel}>Cancel</div>
-          <div className={styles.button} onClick={this.login}>Login</div>
+      <div className={styles.overlay}>
+        <div className={styles.LinkForm}>
+          <div className={styles.heading}>
+            Login with your email
+          </div>
+          <input
+            className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+            placeholder="URL"
+            ref={(f) => { this.urlField = f; }}
+            type="text"
+          />
+          <input
+            className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+            placeholder="Description"
+            ref={(f) => { this.descriptionField = f; }}
+            type="text"
+          />
+          <div className={styles.actionContainer}>
+            <div className={styles.button} onClick={this.props.cancel}>Cancel</div>
+            <div className={styles.button} onClick={this.login}>Login</div>
+          </div>
         </div>
       </div>
     );
--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -9,14 +9,16 @@ import React from 'react';
 import Link from '../Link';
 import styles from './styles.css';
 
-const LinkList = ({ links, topicName }) => (
+const LinkList = ({ links, topicName, children }) => (
   <div className={styles.linkList}>
     <h1>{topicName}</h1>
     {links.map(l => <Link key={l.id} link={l} />)}
+    {children}
   </div>
 );
 
 LinkList.propTypes = {
+  children: React.propTypes.element,
   links: React.PropTypes.arrayOf(
     React.PropTypes.shape({
       voteCount: React.PropTypes.number.isRequired,
