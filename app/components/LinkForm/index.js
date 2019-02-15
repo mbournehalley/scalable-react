0ce729d01819fee202b64a70241a97ebb27fd868
mbournehalley <mbournehalley@gmail.com>
2019-02-14 21:10:33 -0800

    Fleshing out the Link Form Component.

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -7,12 +7,34 @@
 import React from 'react';
 
 import styles from './styles.css';
-
+import classNames from 'classnames';
 class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    cancel: React.PropTypes.func.isRequired,
+  }
+  state = {};
   render() {
     return (
-      <div className={styles.linkForm}>
-       This is the add form
+      <div className={styles.LinkForm}>
+        <div className={styles.heading}>
+          Login with your email
+        </div>
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="URL"
+          ref={(f) => { this.urlField = f; }}
+          type="text"
+        />
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="Description"
+          ref={(f) => { this.descriptionField = f; }}
+          type="text"
+        />
+        <div className={styles.actionContainer}>
+          <div className={styles.button} onClick={this.props.cancel}>Cancel</div>
+          <div className={styles.button} onClick={this.login}>Login</div>
+        </div>
       </div>
     );
   }
